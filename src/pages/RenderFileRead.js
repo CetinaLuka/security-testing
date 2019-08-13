8ccccd22d54d6ccac9bb4bc92401a4d640bac3e2
CetinaLuka <luka.cetina@student.um.si>
2019-08-13 12:33:27 +0200

    removed unused files

--- src/pages/RenderFileRead.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  template: `
-    <div class="bg-theme-feature rounded-lg p-10">
-      <h1>Render: File Read</h1>
-      <div>Read /etc/passwd</div>
-      <pre>{{ typeof require !== 'undefined' && require('fs') ? require('fs').readFileSync('/etc/passwd') : 'NOPE' }}</pre>
-    </div>
-  `
-}
